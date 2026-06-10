1b5205c1c04028f470a98f07c8b863d4c1e346dc
Arnav <umi0236@icloud.com>
2026-06-10 22:46:59 +0900

    Initial Commit

---
+++ DreamDraw/DreamDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamDrawApp.swift
+//  DreamDraw
+//
+//  Created by SUDØ on 6/10/26.
+//
+
+import SwiftUI
+
+@main
+struct DreamDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
